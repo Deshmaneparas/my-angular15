75106fc25168a906af6e99f2193eaf611e4d7e28
Deshmaneparas <141109408+Deshmaneparas@users.noreply.github.com>
2023-08-12 15:10:02 +0530

    5 files modified, 1 added

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -2,9 +2,9 @@ import { Hero } from "./hero";
 
 export const HEROES:Hero[]=[
   {id:1, avtar:'https://www.mansworldindia.com/wp-content/uploads/2022/07/Virat-Kholi-dropped.jpg',name:'virat kholi',field:'cricket',info:'Run Machine'},
-  {id:2, avtar:'https://w0.peakpx.com/wallpaper/360/537/HD-wallpaper-rohit-sharma-best-smile-rohit-sharma-smile-cricketer-hitman-thumbnail.jpg',name:'rohit sharma',field:'cricket',info:'Hitman'},
+  {id:2, avtar:'https://images.news9live.com/wp-content/uploads/2023/06/Rohit-Sharma-West-Indies-series-AP.png?q=50&w=1200',name:'rohit sharma',field:'cricket',info:'Hitman'},
   {id:3, avtar:'https://i.pinimg.com/736x/34/0f/89/340f89bd37ec229a99712233eaae8239.jpg',name:'kl rahul',field:'cricket',info:'captain cool'},
   {id:4, avtar:'https://www.100mbsports.com/wp-content/uploads/2022/07/71114067-e1656671241746.jpg',name:'jasprit bumrah',field:'cricket',info:'yorker king'},
-  {id:5, avtar:'https://ss-i.thgim.com/public/incoming/article38486866.ece/alternates/FREE_1200/IPLDM18631JPG',name:'surya kumar yadav',field:'cricket',info:'SKY'},
+  {id:5, avtar:'https://images.hindustantimes.com/img/2022/11/20/550x309/New-Zealand-India-T20-Cricket-16_1668934621811_1668934621811_1668934639276_1668934639276.jpg',name:'surya kumar yadav',field:'cricket',info:'SKY'},
 
 ];
